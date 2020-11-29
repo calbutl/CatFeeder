fe5b9c792d62a87800a040c793eff42035d99592
Phil Butler <pbphilbutler@gmail.com>
2020-11-29 11:10:42 -0500

    working read and write

--- ImageCaptureTest.py
+++ ImageCaptureTest.py
@@ -4,15 +4,17 @@ from time import sleep, strftime
 
 if __name__ == "__main__":
     fps = 10
-    frame_size = (640,480)
     timestr = strftime("%Y%m%d-%H%M%S")
-    output_file = f"video_{timestr}"
+    output_file = f"images/video_{timestr}.avi"
 
     cap = cv2.VideoCapture(0) # Create video capture object with 0th camera
-    fourcc = cv2.VideoWriter_fourcc(*'X264') # Create VideoWriter format object with smallest file size
+    frame_size = (int(cap.get(3)), int(cap.get(4)))
+
+
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG') # Create VideoWriter format object with smallest file size
     out = cv2.VideoWriter(output_file, fourcc, fps, frame_size) # fps on write and wait on read should match? frame size?
 
-    for _ in range(10):
+    for _ in range(200):
         if(cap.isOpened()):
             ret, frame = cap.read()
             if ret==True:
