fcdd815d57ab4f0500aa3d3531d1f6bd87f82c68
calbutl <ckcalbut@gmail.com>
2020-12-06 23:18:27 -0500

    moved lines around.  no change

--- ImageCaptureTest.py
+++ ImageCaptureTest.py
@@ -4,14 +4,12 @@ from time import sleep, strftime
 
 if __name__ == "__main__":
     fps = 10
-    timestr = strftime("%Y%m%d-%H%M%S")
-    output_file = f"images/video_{timestr}.avi"
-
     cap = cv2.VideoCapture(0) # Create video capture object with 0th camera
     frame_size = (int(cap.get(3)), int(cap.get(4)))
-
-
     fourcc = cv2.VideoWriter_fourcc(*'MJPG') # Create VideoWriter format object with smallest file size
+
+    timestr = strftime("%Y%m%d-%H%M%S")
+    output_file = f"images/video_{timestr}.avi"
     out = cv2.VideoWriter(output_file, fourcc, fps, frame_size) # fps on write and wait on read should match? frame size?
 
     for _ in range(200):
@@ -20,19 +18,6 @@ if __name__ == "__main__":
             if ret==True:
                 out.write(frame)
 
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-# Display video we just saved.
-cap = cv2.VideoCapture(output_file)
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
-
 cap.release()
+out.release()
 cv2.destroyAllWindows()
